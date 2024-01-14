63d9c87332731fa26eb27d6cc51b5505d605191c
Nathan <nathanbarrosnt@gmail.com>
2024-01-14 11:50:23 -0300

    feat: user endpoints

--- src/main/java/com/neidev/picpay/controller/UserController.java
+++ src/main/java/com/neidev/picpay/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.neidev.picpay.controller;
 
+import com.neidev.picpay.domain.core.user.json.UserForm;
+import com.neidev.picpay.domain.core.user.json.UserResponse;
 import com.neidev.picpay.service.impl.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
@@ -12,6 +18,16 @@ public class UserController {
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponse> createUser(@RequestBody @Valid UserForm data) {
+        return new ResponseEntity<>(userService.create(data), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
     }
 
 }
